dbd181181c4819e53fba4b348f4a704c47f94e41
jenniferyao1996 <jenniferyao1996@hotmail.com>
2021-03-17 19:40:32 -0400

    searchbar add background image

--- homepage1.js
+++ homepage1.js
@@ -1,48 +1,38 @@
-    window.onresize = function () {
-      if (window.innerWidth > 800) {
-        const sideBar = document.getElementById("sideBar");
-        sideBar.style.visibility = "visible";
-      };
-    }
-   
-    function showClearIcon() {
-      const searchBtn = document.getElementById("searchKey");
-      if(!searchBtn) { 
-        return ;
-      }
-        
-      const clearIcon = document.getElementById("clearIcon");
-      if (!clearIcon) { 
-        return ;
-      }
+function showClearIcon() {
+  const searchBtn = document.getElementById("searchKey");
+  if (!searchBtn) {
+    return;
+  }
 
-    
-      if(searchBtn.value.trim().length >= 1){
-          clearIcon.style.visibility="visible";
-      } else {
-          clearIcon.style.visibility="hidden";
-      } 
+  const clearIcon = document.getElementById("clearIcon");
+  if (!clearIcon) {
+    return;
+  }
 
-    }
+  if (searchBtn.value.trim().length >= 1) {
+    clearIcon.style.visibility = "visible";
+  } else {
+    clearIcon.style.visibility = "hidden";
+  }
+}
 
+function clearSearchBar() {
+  const searchBtn = document.getElementById("searchKey");
+  const clearIcon = document.getElementById("clearIcon");
+  if (searchBtn) {
+    searchBtn.value = "";
+  }
+  if (clearIcon) {
+    clearIcon.style.visibility = "hidden";
+  }
+}
 
-    function clearSearchBar() {
-      const searchBtn = document.getElementById("searchKey");
-      const clearIcon = document.getElementById("clearIcon");
-      if (searchBtn) {
-        searchBtn.value="";
-      }
-      if (clearIcon) {
-        clearIcon.style.visibility = "hidden";
-      }
-    }
-
-    function toggleSideBar() {
-      const sideBar = document.getElementById("sideBar");
-      let hidden= (sideBar.style.visibility === "hidden");
-      if (hidden) {
-        sideBar.style.visibility = "visible";
-      }  else {
-          sideBar.style.visibility = "hidden";
-      }
-    }
+function toggleSideBar() {
+  const sideBar = document.getElementById("sideBar");
+  let hidden = sideBar.style.visibility === "hidden";
+  if (hidden) {
+    sideBar.style.visibility = "visible";
+  } else {
+    sideBar.style.visibility = "hidden";
+  }
+}
