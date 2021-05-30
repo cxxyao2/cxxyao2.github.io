3a4cf2881be802c38e832a87a3c152ed7b25b43f
cxxyao2 <cxxyao2@gmail.com>
2021-05-30 16:54:53 -0400

    style index.html

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ collapsibles.forEach((item) =>
     this.classList.toggle('collapsible-expanded');
   })
 );
+
+const backToTop = document.getElementById('backToTop');
+backToTop.addEventListener('click', function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 20) {
+    backToTop.style.display = 'inline-block';
+  } else {
+    backToTop.style.display = 'none';
+  }
+});
